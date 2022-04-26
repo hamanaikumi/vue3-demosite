42f64994c12f5ec12c89cdccb5188e3a58199ff0
hamanaikumi <ikumi.hamana@rakus-partners.co.jp>
2022-04-26 13:14:19 +0900

    TOPページ内コンセプトのアニメーション修正

--- src/components/Atoms/AnimationImage2.vue
+++ src/components/Atoms/AnimationImage2.vue
@@ -13,19 +13,19 @@ export default defineComponent({
 img {
   width: 100%;
   height: 100%;
-  opacity: 0.6;
-  transform: rotate(0deg);
+
+  transform: rotate(10deg);
   animation: step 2s steps(1) infinite;
 }
 @keyframes step {
   0% {
-    transform: rotate(0deg);
+    transform: rotate(10deg);
   }
   50% {
-    transform: rotate(-20deg);
+    transform: rotate(-10deg);
   }
   100% {
-    transform: rotate(0deg);
+    transform: rotate(10deg);
   }
 }
 </style>
